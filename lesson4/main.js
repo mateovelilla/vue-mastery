28a26f692fc38108b214567e1ea01df1ec5faa22
Mateo Velilla <teovelilla@gmail.com>
2019-02-23 11:37:03 -0500

    The first commit

---
+++ lesson4/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+	el:"#app",
+	data: {
+		product: 'Socks',
+		image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+		inStock: true,
+		details: ["80% cotton", "20% polyester", "Gender-nautral"],
+		variants:[
+			{
+				variantId: 2234,
+				variantColor: "green"
+			},
+			{
+				variantId: 2235,
+				variantColor: "blue"
+			}
+		],
+		sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+	}
+})
